cdc2db7c44d7caf5acd599d2a980f91af4211add
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2025-03-14 10:08:35 -0300

    Update app.js

--- aula15/src/app.js
+++ aula15/src/app.js
@@ -23,7 +23,7 @@ app.use('/user', userRouter);
 app.use(express.static(path.join(`${__dirname}/public`)));
 
 // aqui é o nome da database
-mongoose.connect('mongodb+srv://alessandra:coder@clustercoder.n6nab.mongodb.net/usersdb') // informamos a database escola
+mongoose.connect('MONGO_URI') // informamos a database escola
   .then(() => {
     console.log('Conectado ao MongoDB com sucesso');
   }).catch((error) => {
