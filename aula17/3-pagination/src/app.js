43b76e476d2c02becf16735d0d6e32462336d3cf
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2025-03-14 10:11:30 -0300

    Update app.js

--- aula17/3-pagination/src/app.js
+++ aula17/3-pagination/src/app.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const usersModel = require('./models/users.model');
 
 const environment = async () => {
-  await mongoose.connect('mongodb+srv://alessandra:coder@clustercoder.n6nab.mongodb.net/usersdb?retryWrites=true&w=majority&appName=ClusterCoder');
+  await mongoose.connect('MONGO_URI');
 
   let users = await usersModel.paginate({gender: 'Female'}, { page: 2, limit: 10 });
 
