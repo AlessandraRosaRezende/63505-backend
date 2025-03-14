7fb32381435cd74ffdb50a5b22500df56a7bd436
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2025-03-14 10:09:08 -0300

    Update app.js

--- aula16/1-exemplo/src/app.js
+++ aula16/1-exemplo/src/app.js
@@ -2,7 +2,7 @@ const userModel = require('./models/user.model');
 const mongoose = require('mongoose');
 
 const environment = async() => {
-  await mongoose.connect('mongodb+srv://alessandra:coder@clustercoder.n6nab.mongodb.net/usersdb?retryWrites=true');
+  await mongoose.connect('MONGO_URI');
 
   let response = await userModel.find({ first_name: 'wanda'}).explain('executionStats');
   console.log(response);
