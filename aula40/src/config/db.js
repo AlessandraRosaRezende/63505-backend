551dc55b4342dfe224e6d68cabba7d865f96719e
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2025-03-18 21:30:35 -0300

    feat: aula40

--- aula40/src/config/db.js
+++ aula40/src/config/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error);
