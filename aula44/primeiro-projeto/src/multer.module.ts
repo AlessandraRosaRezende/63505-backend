4a7836f500a0581909d9f801e825004ba7b1aaf4
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2025-04-01 22:23:23 -0300

    fix path

--- aula44/primeiro-projeto/src/multer.module.ts
+++ aula44/primeiro-projeto/src/multer.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { join } from 'path';
 
 @Module({
   imports: [
     MulterModule.register({
-      dest: './uploads', // Diretório onde os arquivos serão salvos
+      dest: join(__dirname, '..', 'uploads'), // Diretório onde os arquivos serão salvos
     }),
   ],
 })
